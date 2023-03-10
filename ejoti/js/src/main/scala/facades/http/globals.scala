e1cc489b673b781ed8e4a9aa8f9954f4b6f79387
Antoine Doeraene <antoine.doeraene@gmail.com>
2023-03-10 18:23:10 +0100

    ⚡ Improve chunk to uintarray perfs

--- ejoti/js/src/main/scala/facades/http/globals.scala
+++ ejoti/js/src/main/scala/facades/http/globals.scala
@@ -4,10 +4,24 @@ import zio.{Chunk, ZIO}
 import scala.scalajs.js.typedarray.Uint8Array
 import scalajs.js.JSConverters.JSRichIterableOnce
 import scala.scalajs.js.typedarray.TypedArrayBuffer
+import java.nio.{ByteBuffer => JByteBuffer}
+import scala.scalajs.js.typedarray.ArrayBuffer
+import scala.scalajs.js.typedarray.TypedArrayBuffer
+import scala.scalajs.js.typedarray.Uint8Array
+import scala.scalajs.js.typedarray.TypedArrayBufferOps._
 
-def chunkToUint8Array(chunk: Chunk[Byte]): Uint8Array =
-  Uint8Array.from(chunk.toList.toJSArray.map(_.toShort))
-
+def chunkToUint8Array(chunk: Chunk[Byte]): Uint8Array = {
+  import scala.language.unsafeNulls
+  val javaByteBuffer = JByteBuffer.wrap(chunk.toArray)
+  val arrayBuffer =
+    if javaByteBuffer.hasArrayBuffer() then javaByteBuffer.arrayBuffer()
+    else {
+      val ab = new ArrayBuffer(javaByteBuffer.remaining())
+      TypedArrayBuffer.wrap(ab).put(javaByteBuffer)
+      ab
+    }
+  new Uint8Array(arrayBuffer, 0, arrayBuffer.byteLength)
+}
 def uint8ArrayToChunk(uint8Array: Uint8Array): Chunk[Byte] =
   Chunk.fromByteBuffer(TypedArrayBuffer.wrap(uint8Array.buffer))
 
@@ -16,6 +30,6 @@ private[http] def handleNodeResponseFromEjotiResponse(
     response: ejoti.domain.Response
 ): ZIO[Any, Nothing, Unit] = for {
   _ <- ZIO.succeed(jsResponse.writeHead(response.status, response.headers))
-  _ <- response.body.runForeach(chunk => ZIO.succeed(jsResponse.write(chunk)))
+  _ <- response.body.chunks.runForeach(chunk => ZIO.succeed(jsResponse.write(chunk)))
   _ <- ZIO.succeed(jsResponse.end())
 } yield ()
