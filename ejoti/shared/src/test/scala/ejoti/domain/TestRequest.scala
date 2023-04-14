b8c99a7c2daeb27d18596aa2d23f9221dc329974
Antoine Doeraene <antoine.doeraene@gmail.com>
2023-04-14 18:24:21 +0200

    fixed broken test

--- ejoti/shared/src/test/scala/ejoti/domain/TestRequest.scala
+++ ejoti/shared/src/test/scala/ejoti/domain/TestRequest.scala
@@ -2,13 +2,14 @@ package ejoti.domain
 
 import zio.stream.*
 import zio.Chunk
+import ejoti.domain.Header.Headers
 
 object TestRequest {
 
   def get = fromMethod(HttpMethod.GET)
 
   def fromMethod(method: HttpMethod): Request.RawRequest =
-    Request(method, Vector.empty, Nil, Map.empty, "", ZStream.empty)
+    Request(method, Headers.empty, Nil, Map.empty, "", ZStream.empty)
 
   def fromBodyString(body: String): Request.RawRequest = get.withStreamBodyAsString(body)
 
